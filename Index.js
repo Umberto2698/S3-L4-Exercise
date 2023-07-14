16121073fe80f8f3eb1af28aa8965b3da57485c1
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-07-14 17:26:56 +0200

    Added an event to let utent pick cards

--- Index.js
+++ Index.js
@@ -1,12 +1,40 @@
 const tabellone = document.getElementById("tabellone");
 const buttonContainer = document.getElementById("forbutton");
 const estratti = document.getElementById("estratti");
-let exstractedNmb = [];
+const numeroCartelle = document.getElementById("inputCartelle");
+const buttonCartelle = document.getElementById("buttonCartelle");
 
+let cartelleNmb = [];
+const scegliCartelle = (event) => {
+  event.preventDefault();
+  for (let i = 0; i < numeroCartelle.value; i++) {
+    const cartella = document.createElement("div");
+    for (let j = 0; j < 24; j++) {
+      const casella = document.createElement("div");
+      const nmbLocation = document.createElement("p");
+      let randomNmb = Math.floor(Math.random() * 76 + 1);
+      if (cartelleNmb.some((n) => n === randomNmb)) {
+        j--;
+        continue;
+      } else {
+        cartelleNmb.push(randomNmb);
+        nmbLocation.innerText = `${randomNmb}`;
+        casella.classList.add("casella");
+        nmbLocation.classList.add("cella");
+        casella.appendChild(nmbLocation);
+        cartella.appendChild(casella);
+        estratti.appendChild(cartella);
+      }
+    }
+    cartelleNmb = [];
+  }
+};
+
+let exstractedNmb = [];
 const exstraction = () => {
   estratti.innerHTML = "";
   let randomNmb = Math.floor(Math.random() * 76 + 1);
-  for (i = 0; i < exstractedNmb.length; i++) {
+  for (let i = 0; i < exstractedNmb.length; i++) {
     if (exstractedNmb[i] === randomNmb) {
       exstraction();
       return;
@@ -20,7 +48,7 @@ const exstraction = () => {
   estratti.appendChild(p);
 };
 
-for (i = 0; i < 76; i++) {
+for (let i = 0; i < 76; i++) {
   const casella = document.createElement("div");
   casella.classList.add("casella");
   const nmbLocation = document.createElement("p");
